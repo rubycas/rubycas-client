3b80b46149fd96750171c2c73eee3ff6215dbb6e
matt.zukowski <matt.zukowski@e5935e18-7a1f-0410-a9c8-d10ac5b774bc>
2006-10-16 14:42:52 +0000

    added weekly rotation to cas filter log file
    git-svn-id: https://rubycas-client.googlecode.com/svn/trunk/rubycas-client@12 e5935e18-7a1f-0410-a9c8-d10ac5b774bc

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ require 'cas_proxy_callback_controller'
 #CAS::Filter.logger = RAILS_DEFAULT_LOGGER if !RAILS_DEFAULT_LOGGER.nil?
 #CAS::Filter.logger = config.logger if !config.logger.nil?
 
-CAS::Filter.logger = Logger.new "#{RAILS_ROOT}/log/cas_filter.log"
+CAS::Filter.logger = Logger.new("#{RAILS_ROOT}/log/cas_filter.log", 'weekly')
 CAS::Filter.logger.level = Logger::DEBUG
 
 #class ActionController::Base
