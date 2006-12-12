52d35d29495d995fb41c195d5acf63379498fc91
matt.zukowski <matt.zukowski@e5935e18-7a1f-0410-a9c8-d10ac5b774bc>
2006-12-12 19:06:29 +0000

    bugging level is now set to WARN when RAILS_ENV is production
    git-svn-id: https://rubycas-client.googlecode.com/svn/trunk/rubycas-client@42 e5935e18-7a1f-0410-a9c8-d10ac5b774bc

--- init.rb
+++ init.rb
@@ -8,7 +8,11 @@ require 'cas_proxy_callback_controller'
 CAS::Filter.logger = CAS::Logger.new("#{RAILS_ROOT}/log/cas_client_#{RAILS_ENV}.log", 'weekly')
 CAS::Filter.logger.formatter = CAS::Logger::Formatter.new
 
-CAS::Filter.logger.level = Logger::DEBUG
+if RAILS_ENV == "production"
+  CAS::Filter.logger.level = Logger::WARN
+else
+  CAS::Filter.logger.level = Logger::DEBUG
+end
 
 
 #class ActionController::Base
