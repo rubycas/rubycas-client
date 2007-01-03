35862ce6e045ab15d2bbdb8fb89e4565274c3f7f
matt.zukowski <matt.zukowski@e5935e18-7a1f-0410-a9c8-d10ac5b774bc>
2007-01-03 18:54:37 +0000

    switched to file-size based log rotation due to weird shifting problem with weekly rotation
    git-svn-id: https://rubycas-client.googlecode.com/svn/trunk/rubycas-client@44 e5935e18-7a1f-0410-a9c8-d10ac5b774bc

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ require 'cas_proxy_callback_controller'
 #CAS::Filter.logger = RAILS_DEFAULT_LOGGER if !RAILS_DEFAULT_LOGGER.nil?
 #CAS::Filter.logger = config.logger if !config.logger.nil?
 
-CAS::Filter.logger = CAS::Logger.new("#{RAILS_ROOT}/log/cas_client_#{RAILS_ENV}.log", 'weekly')
+CAS::Filter.logger = CAS::Logger.new("#{RAILS_ROOT}/log/cas_client_#{RAILS_ENV}.log", 1024000)
 CAS::Filter.logger.formatter = CAS::Logger::Formatter.new
 
 if RAILS_ENV == "production"
