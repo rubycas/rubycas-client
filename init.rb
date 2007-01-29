48f33b05734fe2d7a0d2429794b6c1feed93e345
matt.zukowski <matt.zukowski@e5935e18-7a1f-0410-a9c8-d10ac5b774bc>
2007-01-29 20:36:22 +0000

    removed log rotation via Logger because it was causing problems under multi-instance clustered mongrel deployment
    git-svn-id: https://rubycas-client.googlecode.com/svn/trunk/rubycas-client@54 e5935e18-7a1f-0410-a9c8-d10ac5b774bc

--- init.rb
+++ init.rb
@@ -5,13 +5,13 @@ require 'cas_proxy_callback_controller'
 #CAS::Filter.logger = RAILS_DEFAULT_LOGGER if !RAILS_DEFAULT_LOGGER.nil?
 #CAS::Filter.logger = config.logger if !config.logger.nil?
 
-CAS::Filter.logger = CAS::Logger.new("#{RAILS_ROOT}/log/cas_client_#{RAILS_ENV}.log", 1024000)
+CAS::Filter.logger = CAS::Logger.new("#{RAILS_ROOT}/log/cas_client_#{RAILS_ENV}.log")
 CAS::Filter.logger.formatter = CAS::Logger::Formatter.new
 
 #if RAILS_ENV == "production"
 #  CAS::Filter.logger.level = Logger::WARN
 #else
-  CAS::Filter.logger.level = Logger::DEBUG
+#  CAS::Filter.logger.level = Logger::DEBUG
 #end
 
 
