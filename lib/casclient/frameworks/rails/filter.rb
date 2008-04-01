290bd5efab1e8807c4544485491fdc8b7c31e3b8
matt.zukowski <matt.zukowski@e5935e18-7a1f-0410-a9c8-d10ac5b774bc>
2008-04-01 19:49:25 +0000

    Added redirection loop interception: The client now logs a warning message when it   believes that it is stuck in a redirection loop with the CAS server. If more   than three of these redirects occur within one second, the client will   redirect back to the login page with renew=1, forcing the user to try   authenticating again.
    git-svn-id: https://rubycas-client.googlecode.com/svn/trunk/rubycas-client@156 e5935e18-7a1f-0410-a9c8-d10ac5b774bc

--- lib/casclient/frameworks/rails/filter.rb
+++ lib/casclient/frameworks/rails/filter.rb
@@ -140,7 +140,17 @@ module CASClient
             
             if controller.session[:previous_redirect_to_cas] &&
                 controller.session[:previous_redirect_to_cas] > (Time.now - 1.second)
-              log.warn("Previous redirect to the CAS server was less than a second ago. The client may be stuck in a redirection loop!")
+              log.warn("Previous redirect to the CAS server was less than a second ago. The client at #{controller.request.remote_ip.inspect} may be stuck in a redirection loop!")
+              controller.session[:cas_validation_retry_count] ||= 0
+              
+              if controller.session[:cas_validation_retry_count] > 3
+                log.error("Redirection loop intercepted. Client at #{controller.request.remote_ip.inspect} will be redirected back to login page and forced to renew authentication.")
+                redirect_url += "&renew=1&redirection_loop_intercepted=1"
+              end
+              
+              controller.session[:cas_validation_retry_count] += 1
+            else
+              controller.session[:cas_validation_retry_count] = 0
             end
             controller.session[:previous_redirect_to_cas] = Time.now
             
