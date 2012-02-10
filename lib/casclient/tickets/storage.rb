05ceb08969d9bf95df3e996db11423184e903adf
Bryan Larsen <bryan@larsen.st>
2012-02-10 13:57:32 -0500

    stick ticket storage in /tmp

--- lib/casclient/tickets/storage.rb
+++ lib/casclient/tickets/storage.rb
@@ -71,7 +71,8 @@ module CASClient
 
         def initialize(config={})
           config ||= {}
-          default_tmp_dir = defined?(Rails.root) ? "#{Rails.root}/tmp" : "#{Dir.pwd}/tmp"
+          #default_tmp_dir = defined?(Rails.root) ? "#{Rails.root}/tmp" : "#{Dir.pwd}/tmp"
+          default_tmp_dir = "/tmp"
           @tmp_dir = config[:storage_dir] || default_tmp_dir
           @service_session_lookup_dir = config[:service_session_lookup_dir] || "#{@tmp_dir}/sessions"
           @pgt_store_path = config[:pgt_store_path] || "#{@tmp_dir}/cas_pgt.pstore"
