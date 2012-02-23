ca7f6d604767559e9a888364a1ba873eba98f6ba
Matt Campbell <matt@soupmatt.com>
2012-02-23 12:09:44 -0600

    Cleanup spec_helper [ci-skip]

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,16 +16,14 @@ RSpec.configure do |config|
   config.mock_framework = :rspec
   config.include ActionControllerHelpers
 
-  config.before(:all) do
-  end
-
-  config.after(:suite) do
-    ActiveRecordHelpers.teardown_active_record
-  end
   config.before(:suite) do
     ActiveRecordHelpers.setup_active_record
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.after(:suite) do
+    ActiveRecordHelpers.teardown_active_record
   end
 
   config.before(:each) do
