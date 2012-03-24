a93658931e448e8141bbaf1ae33230dc39505343
Matt Campbell <matt@soupmatt.com>
2012-03-23 23:37:57 -0500

    Handle boolean values and raw values when parsing extra attributes
    Fix #38
    add :raw mode to extra attribute parsing

--- lib/casclient/responses.rb
+++ lib/casclient/responses.rb
@@ -2,39 +2,39 @@ module CASClient
   module XmlResponse
     attr_reader :xml, :parse_datetime
     attr_reader :failure_code, :failure_message
-    
+
     def check_and_parse_xml(raw_xml)
       begin
-        doc = REXML::Document.new(raw_xml)
+        doc = REXML::Document.new(raw_xml, :raw => :all)
       rescue REXML::ParseException => e
         raise BadResponseException, 
           "MALFORMED CAS RESPONSE:\n#{raw_xml.inspect}\n\nEXCEPTION:\n#{e}"
       end
-      
+
       unless doc.elements && doc.elements["cas:serviceResponse"]
         raise BadResponseException, 
           "This does not appear to be a valid CAS response (missing cas:serviceResponse root element)!\nXML DOC:\n#{doc.to_s}"
       end
-      
+
       return doc.elements["cas:serviceResponse"].elements[1]
     end
-    
+
     def to_s
       xml.to_s
     end
   end
-  
+
   # Represents a response from the CAS server to a 'validate' request
   # (i.e. after validating a service/proxy ticket).
   class ValidationResponse
     include XmlResponse
-    
+
     attr_reader :protocol, :user, :pgt_iou, :proxies, :extra_attributes
-    
+
     def initialize(raw_text, options={})
       parse(raw_text, options)
     end
-    
+
     def parse(raw_text, options)
       raise BadResponseException, 
         "CAS response is empty/blank." if raw_text.blank?
@@ -45,17 +45,17 @@ module CASClient
         @user = $~[2]
         return
       end
-      
+
       @xml = check_and_parse_xml(raw_text)
-      
+
       # if we got this far then we've got a valid XML response, so we're doing CAS 2.0
       @protocol = 2.0
-      
+
       if is_success?
         cas_user = @xml.elements["cas:user"]
         @user = cas_user.text.strip if cas_user
         @pgt_iou =  @xml.elements["cas:proxyGrantingTicket"].text.strip if @xml.elements["cas:proxyGrantingTicket"]
-        
+
         proxy_els = @xml.elements.to_a('//cas:authenticationSuccess/cas:proxies/cas:proxy')
         if proxy_els.size > 0
           @proxies = []
@@ -63,7 +63,7 @@ module CASClient
             @proxies << el.text
           end
         end
-        
+
         @extra_attributes = {}
         @xml.elements.to_a('//cas:authenticationSuccess/cas:attributes/* | //cas:authenticationSuccess/*[local-name() != \'proxies\' and local-name() != \'proxyGrantingTicket\' and local-name() != \'user\' and local-name() != \'attributes\']').each do |el|
           inner_text = el.cdatas.length > 0 ? el.cdatas.join('') : el.text
@@ -74,7 +74,7 @@ module CASClient
           end
           @extra_attributes.merge! name => inner_text
         end
-        
+
         # unserialize extra attributes
         @extra_attributes.each do |k, v|
           @extra_attributes[k] = parse_extra_attribute_value(v, options[:encode_extra_attributes_as])
@@ -90,26 +90,28 @@ module CASClient
 
     def parse_extra_attribute_value(value, encode_extra_attributes_as)
       attr_value = if value.blank?
-        nil
-      elsif !encode_extra_attributes_as
-        begin
-            YAML.load(value)
-          rescue ArgumentError
-            raise ArgumentError, "Did not find :encode_extra_attributes_as config parameter, hence default encoding scheme is YAML but CAS response recieved in encoded differently "
-          end
-      else
-        if encode_extra_attributes_as == :json
-          begin
-            JSON.parse(value)
-          rescue JSON::ParserError
-            value
-          end
-        else
-          YAML.load(value)
-        end
-      end
+         nil
+       elsif !encode_extra_attributes_as
+         begin
+           YAML.load(value)
+         rescue ArgumentError => e
+           raise ArgumentError, "Error parsing extra attribute with value #{value} as YAML: #{e}"
+         end
+       else
+         if encode_extra_attributes_as == :json
+           begin
+             JSON.parse(value)
+           rescue JSON::ParserError
+             value
+           end
+         elsif encode_extra_attributes_as == :raw
+           value
+         else
+           YAML.load(value)
+         end
+       end
 
-      unless (attr_value.kind_of? Enumerable) || attr_value.nil?
+      unless attr_value.kind_of?(Enumerable) || attr_value.kind_of?(TrueClass) || attr_value.kind_of?(FalseClass) || attr_value.nil?
         attr_value.to_s
       else
         attr_value
@@ -119,30 +121,30 @@ module CASClient
     def is_success?
       (instance_variable_defined?(:@valid) &&  @valid) || (protocol > 1.0 && xml.name == "authenticationSuccess")
     end
-    
+
     def is_failure?
       (instance_variable_defined?(:@valid) && !@valid) || (protocol > 1.0 && xml.name == "authenticationFailure" )
     end
   end
-  
+
   # Represents a response from the CAS server to a proxy ticket request 
   # (i.e. after requesting a proxy ticket).
   class ProxyResponse
     include XmlResponse
-    
+
     attr_reader :proxy_ticket
-    
+
     def initialize(raw_text, options={})
       parse(raw_text)
     end
-    
+
     def parse(raw_text)
       raise BadResponseException, 
         "CAS response is empty/blank." if raw_text.blank?
       @parse_datetime = Time.now
-      
+
       @xml = check_and_parse_xml(raw_text)
-      
+
       if is_success?
         @proxy_ticket = @xml.elements["cas:proxyTicket"].text.strip if @xml.elements["cas:proxyTicket"]
       elsif is_failure?
@@ -152,43 +154,43 @@ module CASClient
         # this should never happen, since the response should already have been recognized as invalid
         raise BadResponseException, "BAD CAS RESPONSE:\n#{raw_text.inspect}\n\nXML DOC:\n#{doc.inspect}"
       end
-      
+
     end
-    
+
     def is_success?
       xml.name == "proxySuccess"
     end
-    
+
     def is_failure?
       xml.name == "proxyFailure"
     end
   end
-  
+
   # Represents a response from the CAS server to a login request 
   # (i.e. after submitting a username/password).
   class LoginResponse
     attr_reader :tgt, :ticket, :service_redirect_url
     attr_reader :failure_message
-    
+
     def initialize(http_response = nil, options={})
       parse_http_response(http_response) if http_response
     end
-    
+
     def parse_http_response(http_response)
       header = http_response.to_hash
-      
+
       # FIXME: this regexp might be incorrect...
       if header['set-cookie'] && 
-          header['set-cookie'].first && 
-          header['set-cookie'].first =~ /tgt=([^&]+);/
+        header['set-cookie'].first && 
+        header['set-cookie'].first =~ /tgt=([^&]+);/
         @tgt = $~[1]
       end
-    
+
       location = header['location'].first if header['location'] && header['location'].first
       if location =~ /ticket=([^&]+)/
         @ticket = $~[1]
       end
-      
+
       if not ((http_response.kind_of?(Net::HTTPSuccess) || http_response.kind_of?(Net::HTTPFound)) && @ticket.present?)
         @failure = true
         # Try to extract the error message -- this only works with RubyCAS-Server.
@@ -200,19 +202,19 @@ module CASClient
           @failure_message = body
         end
       end
-      
+
       @service_redirect_url = location
     end
-    
+
     def is_success?
       !@failure && !ticket.blank?
     end
-    
+
     def is_failure?
       @failure == true
     end
   end
-  
+
   class BadResponseException < CASException
   end
 end
--- spec/casclient/validation_response_spec.rb
+++ spec/casclient/validation_response_spec.rb
@@ -2,6 +2,72 @@ require 'spec_helper'
 require 'casclient/responses.rb'
 
 describe CASClient::ValidationResponse do
+  context "when parsing extra attributes as raw" do
+    let(:response_text) do
+<<RESPONSE_TEXT
+<cas:serviceResponse xmlns:cas="http://www.yale.edu/tp/cas">
+  <cas:authenticationSuccess>
+    <cas:attributes>
+      <cas:name>Jimmy Bob</cas:name>
+      <cas:status><![CDATA[stuff
+]]></cas:status>
+      <cas:yaml><![CDATA[--- true
+]]></cas:yaml>
+      <cas:json><![CDATA[{"id":10529}]]></cas:json>
+    </cas:attributes>
+  </cas:authenticationSuccess>
+</cas:serviceResponse>
+RESPONSE_TEXT
+    end
+
+    subject { CASClient::ValidationResponse.new response_text, :encode_extra_attributes_as => :raw }
+
+    it "sets text attributes to their string value" do
+      subject.extra_attributes["name"].should == "Jimmy Bob"
+    end
+
+    it "preserves whitespace for CDATA" do
+      subject.extra_attributes["status"].should == "stuff\n"
+    end
+
+    it "passes yaml through as is" do
+      subject.extra_attributes["yaml"].should == "--- true\n"
+    end
+    it "passes json through as is" do
+      subject.extra_attributes["json"].should == "{\"id\":10529}"
+    end
+  end
+
+  context "when parsing extra attributes as yaml" do
+    let(:response_text) do
+<<RESPONSE_TEXT
+<cas:serviceResponse xmlns:cas="http://www.yale.edu/tp/cas">
+  <cas:authenticationSuccess>
+    <cas:attributes>
+      <cas:name>Jimmy Bob</cas:name>
+      <cas:status><![CDATA[stuff
+]]></cas:status>
+      <cas:truthy><![CDATA[--- true
+]]></cas:truthy>
+      <cas:falsy><![CDATA[#{false.to_yaml}]]></cas:falsy>
+    </cas:attributes>
+  </cas:authenticationSuccess>
+</cas:serviceResponse>
+RESPONSE_TEXT
+    end
+
+    subject { CASClient::ValidationResponse.new response_text, :encode_extra_attributes_as => :yaml }
+
+    it "sets text attributes to their string value" do
+      subject.extra_attributes["name"].should == "Jimmy Bob"
+    end
+
+    it "sets the value of boolean attributes to their boolean value" do
+      subject.extra_attributes["falsy"].should == false
+      subject.extra_attributes["truthy"].should == true
+    end
+  end
+
   context "when parsing extra attributes as JSON" do
     let(:response_text) do
 <<RESPONSE_TEXT
@@ -12,12 +78,12 @@ describe CASClient::ValidationResponse do
       <cas:last_name>92.5</cas:last_name>
       <cas:mobile_phone></cas:mobile_phone>
       <cas:global_roles><![CDATA[]]></cas:global_roles>
-      <cas:foo_data> <![CDATA[[{"id":10529}]]]></cas:foo_data>
-      <cas:food_data> <![CDATA[{"id":10529}]]></cas:food_data>
+      <cas:foo_data><![CDATA[[{"id":10529}]]]></cas:foo_data>
+      <cas:food_data><![CDATA[{"id":10529}]]></cas:food_data>
       <cas:allegedly_yaml>- 10</cas:allegedly_yaml>
-      <cas:truthy> <![CDATA[--- true
+      <cas:truthy><![CDATA[--- true
 ]]></cas:truthy>
-      <cas:falsy> <![CDATA[--- false
+      <cas:falsy><![CDATA[--- false
 ]]></cas:falsy>
     </cas:attributes>
   </cas:authenticationSuccess>
@@ -54,14 +120,9 @@ RESPONSE_TEXT
     it "sets the value of attributes which are not valid JSON but are valid YAML to their literal value" do
       subject.extra_attributes["allegedly_yaml"].should == '- 10'
     end
-
-    it "sets the value of boolean attributes to their boolean value" do
-      subject.extra_attributes["truthy"].should == true
-      subject.extra_attributes["falsy"].should == false
-    end
   end
 
-  context "When parsing response with cas:attribute style extra attributes" do
+  context "When parsing extra attributes from xml attributes" do
     let(:response_text) do
 <<RESPONSE_TEXT
 <?xml version="1.0" encoding="UTF-8"?>
