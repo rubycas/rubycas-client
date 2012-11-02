902964881fbb3ca514b6f88a7c1b9d40b8d7c022
Tyler Pickett <t.pickett66@gmail.com>
2012-11-02 12:52:21 -0500

    hack our app's test environment to work both ways
    Signed-off-by: Tyler Pickett <t.pickett66@gmail.com>

--- spec/dummy/config/environments/test.rb
+++ spec/dummy/config/environments/test.rb
@@ -1,37 +1,9 @@
-Dummy::Application.configure do
-  # Settings specified here will take precedence over those in config/application.rb
-
-  # The test environment is used exclusively to run your application's
-  # test suite. You never need to work with it otherwise. Remember that
-  # your test database is "scratch space" for the test suite and is wiped
-  # and recreated between test runs. Don't rely on the data there!
-  config.cache_classes = true
-
-  # Configure static asset server for tests with Cache-Control for performance
-  config.serve_static_assets = true
-  config.static_cache_control = "public, max-age=3600"
-
-  # Log error messages when you accidentally call methods on nil
-  config.whiny_nils = true
-
-  # Show full error reports and disable caching
-  config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
-
-  # Raise exceptions instead of rendering exception templates
-  config.action_dispatch.show_exceptions = false
-
-  # Disable request forgery protection in test environment
-  config.action_controller.allow_forgery_protection    = false
-
-  # Tell Action Mailer not to deliver emails to the real world.
-  # The :test delivery method accumulates sent emails in the
-  # ActionMailer::Base.deliveries array.
-  config.action_mailer.delivery_method = :test
-
-  # Raise exception on mass assignment protection for Active Record models
-  config.active_record.mass_assignment_sanitizer = :strict if ActiveRecord::Base.respond_to?(:mass_assignment_sanitizer=)
-
-  # Print deprecation notices to the stderr
-  config.active_support.deprecation = :stderr
+# Since there was a massive shift in how initialization is done for  Rails 3.x
+# we have to do some hackety hackety hacks here to make everything line up
+if defined? RAILS_GEM_VERSION
+  eval(File.read(File.expand_path('../real_test.rb',__FILE__)))
+else
+  Dummy::Application.configure do
+    eval(File.read(File.expand_path('../real_test.rb',__FILE__)))
+  end
 end
