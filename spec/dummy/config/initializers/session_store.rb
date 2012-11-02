96e61dd756a3e93a6e8ce8a330442554405af71c
Tyler Pickett <t.pickett66@gmail.com>
2012-11-02 12:52:54 -0500

    do our initialization both ways...
    Signed-off-by: Tyler Pickett <t.pickett66@gmail.com>

--- spec/dummy/config/initializers/session_store.rb
+++ spec/dummy/config/initializers/session_store.rb
@@ -1,6 +1,13 @@
 # Be sure to restart your server when you modify this file.
 
-Dummy::Application.config.session_store :cookie_store, :key => '_dummy_session'
+if defined? Dummy
+  Dummy::Application.config.session_store :cookie_store, :key => '_dummy_session'
+else
+  ActionController::Base.session = {
+    :key         => '_dummy_session',
+    :secret      => '4de43a1f7e9886181978771e9280d7cb3ce295dffd7b8447040fd1ac66825cbe04675128d6897924d1607df13039e11ab167dad35c30c4d15cbf4de56459e701'
+  }
+end
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
