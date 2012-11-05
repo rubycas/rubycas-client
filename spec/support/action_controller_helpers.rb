9d150564d6981e966f443935a3886c611ed45af3
Tyler Pickett <t.pickett66@gmail.com>
2012-11-05 15:18:28 -0600

    deal with the moronic way Rails 2.3 checks the query string
    Signed-off-by: Tyler Pickett <t.pickett66@gmail.com>

--- spec/support/action_controller_helpers.rb
+++ spec/support/action_controller_helpers.rb
@@ -27,6 +27,12 @@ module ActionControllerHelpers
   def build_request_for(request_env)
     if is_rails2?
       request = ActionController::TestRequest.new(request_env)
+      # because an empty string returns false for #present?
+      # we have to ensure there is a valid value for 'REQUEST_URI'
+      uri = "http://testhost.local"
+      query_string = request.env['QUERY_STRING']
+      uri << (query_string ? "?#{query_string}" : '')
+      request.env['REQUEST_URI'] = uri
       request.query_parameters = request.GET
     else
       request = ActionDispatch::TestRequest.new(request_env)
