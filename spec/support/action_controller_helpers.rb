24f81762d344b57c9ce9ea81896a44dbf03b4291
Tyler Pickett <t.pickett66@gmail.com>
2012-11-06 13:50:55 -0600

    all specs passing without mocked controller, removing controller mock
    Signed-off-by: Tyler Pickett <t.pickett66@gmail.com>

--- spec/support/action_controller_helpers.rb
+++ spec/support/action_controller_helpers.rb
@@ -51,27 +51,6 @@ module ActionControllerHelpers
       controller.send(:initialize_current_url)
     end
   end
-=begin
-  def mock_controller_with_session(request = nil, session={})
-
-    query_parameters = {:ticket => "bogusticket", :renew => false}
-    parameters = query_parameters.dup
-
-    #TODO this really need to be replaced with a "real" rails controller
-    request ||= mock_post_request
-    request.stub(:query_parameters) {query_parameters}
-    request.stub(:path_parameters) {{}}
-    controller = double("Controller")
-    controller.stub(:session) {session}
-    controller.stub(:request) {request}
-    controller.stub(:url_for) {"bogusurl"}
-    controller.stub(:query_parameters) {query_parameters}
-    controller.stub(:path_parameters) {{}}
-    controller.stub(:parameters) {parameters}
-    controller.stub(:params) {parameters}
-    controller
-  end
-=end
 
   def mock_post_request
       mock_request = double("request")
