6fb49f573f31e6e3e2817c998d03dcac81752928
Michael Mell <mike.mell@nthwave.net>
2013-08-27 12:12:21 -0700

    add rack proxy filter

---
+++ lib/casclient/frameworks/rack/proxy_filter.rb
@@ -0,0 +1,82 @@
+module CASClient
+  module Frameworks
+    module Rack
+
+      class Response
+        def initialize(user = nil, extra_attributes = nil, *errors)
+          @user, @extra_attributes, @errors = user, extra_attributes, errors
+        end
+
+        def valid?
+          @errors.empty?
+        end
+
+        def to_s
+          "User: #{@user}, Attributes: #{@extra_attributes}, Valid?: #{valid?}, Errors: #{@errors}"
+        end
+
+        def error_messages
+          @errors.join(", ")
+        end
+      end
+
+
+      # A simplified filter that only manages Proxy requests
+      class ProxyFilter
+
+        cattr_reader :config, :log, :client
+
+        @@config = nil
+        @@client = nil
+        @@log = nil
+
+        class << self
+          def filter(req)
+            raise "Cannot use the CASClient filter because it has not yet been configured." if config.nil?
+            begin
+              if st = read_ticket(req)
+                client.validate_service_ticket(st)
+
+                if st.is_valid? # st.extra_attributes
+                  if st.pgt_iou
+                    if client.retrieve_proxy_granting_ticket(st.pgt_iou)
+                      return Response.new(st.user.dup, HashWithIndifferentAccess.new(st.extra_attributes))
+                    end
+
+                    log.error("Failed to retrieve a PGT for PGT IOU #{st.pgt_iou}!")
+                  end
+                end
+              end
+            rescue OpenSSL::SSL::SSLError => err
+              Response.new(nil, nil, "OpenSSL::SSL::SSLError #{err}")
+            end
+
+            Response.new(nil, nil, "unauthorized!")
+          end
+
+          def configure(config)
+            @@config = config
+            @@config[:logger] = ::App.logger unless @@config[:logger]
+            @@client = CASClient::Client.new(config)
+            @@log = client.log
+          end
+
+          private
+
+          def read_ticket(req)
+            ticket = req.params[:ticket]
+            return nil unless ticket
+            log.debug("Request contains ticket #{ticket.inspect}.")
+            if ticket =~ /^PT-/
+              ProxyTicket.new(ticket, config[:service_url], req.params[:renew])
+            else
+              ServiceTicket.new(ticket, config[:service_url], req.params[:renew])
+            end
+          end
+
+        end
+      end
+
+    end
+  end
+end
