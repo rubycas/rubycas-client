f1b17063b824565fef6a6c6259ad484885847042
dima <pronix.service@gmail.com>
2013-10-31 15:29:38 +0100

    validate ticket without consume

--- lib/casclient/tickets.rb
+++ lib/casclient/tickets.rb
@@ -3,34 +3,34 @@ module CASClient
   class ServiceTicket
     attr_reader :ticket, :service, :renew
     attr_accessor :user, :extra_attributes, :pgt_iou, :success, :failure_code, :failure_message
-    
+
     def initialize(ticket, service, renew = false)
       @ticket = ticket
       @service = service
       @renew = renew
     end
-    
+
     def is_valid?
       success
     end
-    
+
     def has_been_validated?
       not user.nil?
     end
   end
-  
+
   # Represents a CAS proxy ticket.
   class ProxyTicket < ServiceTicket
   end
-  
+
   class ProxyGrantingTicket
     attr_reader :ticket, :iou
-    
+
     def initialize(ticket, iou)
       @ticket = ticket
       @iou = iou
     end
-    
+
     def to_s
       ticket
     end
